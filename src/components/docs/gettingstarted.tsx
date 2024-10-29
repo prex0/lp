d5555d3916b59424aa5e52bfc443d89dcc86ef74
syuhei176 <syuhei176@gmail.com>
2024-10-30 02:24:48 +0900

    update start

--- src/components/docs/gettingstarted.tsx
+++ src/components/docs/gettingstarted.tsx
@@ -14,7 +14,7 @@ export function GettingStartedPage() {
               Installation
             </h1>
             <p className="text-gray-400 text-lg">
-              Install and configure Prex with Vite.
+              Install and configure Prex with Next.js.
             </p>
           </div>
 
@@ -23,64 +23,115 @@ export function GettingStartedPage() {
               Create project
             </h2>
             <p className="text-gray-300">
-              Start by creating a new Vite project:
+              Start by creating a new Prex project:
             </p>
             <pre className="bg-gray-900 p-4 rounded-lg">
               <code className="text-gray-300">
-                npm create vite@latest my-app -- --template react-ts
+                npm create prex@latest
               </code>
             </pre>
           </div>
 
           <div className="space-y-4">
-            <h2 className="text-2xl font-semibold text-white" id="add-dependencies">
-              Add dependencies
+            <h2 className="text-2xl font-semibold text-white" id="manual-installation">
+              Manual installation
             </h2>
             <p className="text-gray-300">
-              Install the required dependencies:
+              Clone the Prex repository and install the dependencies:
+            </p>
+          </div>
+
+          <div className="space-y-4">
+            <h3 className="text-xl font-semibold text-white" id="add-dependencies">
+              Set up shadcn.ui
+            </h3>
+            <p className="text-gray-300">
+              Set up shadcn and add the components you need:
             </p>
             <pre className="bg-gray-900 p-4 rounded-lg">
               <code className="text-gray-300">
-                cd my-app
-                npm install @prex/core @prex/ui tailwindcss
+                {`npx shadcn@latest init
+npx shadcn@latest add button select input label dialog tooltip`}
               </code>
             </pre>
           </div>
 
           <div className="space-y-4">
-            <h2 className="text-2xl font-semibold text-white" id="configure-tailwind">
-              Configure Tailwind CSS
-            </h2>
+            <h3 className="text-xl font-semibold text-white" id="configure-tailwind">
+              Install Prex
+            </h3>
             <p className="text-gray-300">
               Run the following command to create a tailwind.config.js file:
             </p>
             <pre className="bg-gray-900 p-4 rounded-lg">
               <code className="text-gray-300">
-                npx tailwindcss init -p
+                npm install @prex0/uikit
               </code>
             </pre>
           </div>
 
           <div className="space-y-4">
-            <h2 className="text-2xl font-semibold text-white" id="update-tailwind-config">
-              Update tailwind.config.js
-            </h2>
+            <h3 className="text-xl font-semibold text-white" id="update-tailwind-config">
+              Add Providers
+            </h3>
             <p className="text-gray-300">
-              Add the following to your tailwind.config.js file:
+              Add the following to your ./app/providers.tsx file:
             </p>
             <pre className="bg-gray-900 p-4 rounded-lg">
               <code className="text-gray-300">
-                {`/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}`}
+                {`'use client';
+import { PrexUIKitProvider, USDC_TOKEN_ARBITRUM, WETH_TOKEN_ARBITRUM } from "@prex0/uikit";
+
+const TOKENS = [USDC_TOKEN_ARBITRUM, WETH_TOKEN_ARBITRUM];
+
+/**
+ * Providers are used to wrap the app in the PrexUIKitProvider
+ */
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrexUIKitProvider
+      // Arbitrum
+      chainId={42161}
+      policyId={process.env.NEXT_PUBLIC_POLICY_ID || 'test'}
+      apiKey={process.env.NEXT_PUBLIC_API_KEY || 'test'}
+      tokens={TOKENS}
+      dryRun={process.env.NEXT_PUBLIC_DRY_RUN === 'true'}
+  >
+      {children}
+    </PrexUIKitProvider>
+  );
+}
+`}
+              </code>
+            </pre>
+          </div>
+
+
+
+          <div className="space-y-4">
+            <h3 className="text-xl font-semibold text-white" id="update-layout">
+              Update layout
+            </h3>
+            <p className="text-gray-300">
+              Update the following in your ./app/layout.tsx file:
+            </p>
+            <pre className="bg-gray-900 p-4 rounded-lg">
+              <code className="text-gray-300">
+                {`
+import { Providers } from './providers';
+
+...
+
+<html lang="en">
+  <body
+    className={\`\${geistSans.variable} \${geistMono.variable} antialiased dark\`}
+  >
+    <Providers>
+      {children}
+    </Providers>
+  </body>
+</html>
+`}
               </code>
             </pre>
           </div>
