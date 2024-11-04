68ec1a261fbe06a92519fddc9eb20dc691de12cc
syuhei176 <syuhei176@gmail.com>
2024-11-04 11:56:46 +0900

    Prevent zooming on touch interactions

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,18 +1,17 @@
-"use client";
-
 import React from "react";
+import { Metadata } from "next";
 import { DocsLayout } from "./docs-layout";
-import { SidebarProvider, SidebarTrigger } from "../../components/ui/sidebar";
-import { DocSidebar } from "../../components/docs/doc-sidebar";
+
+export const metadata: Metadata = {
+  title: "Docs - Prex -",
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
+  description:
+    "Prex is a platform for building decentralized applications (DApps) on the Ethereum blockchain. It provides a suite of tools and services to streamline the development process and make it more efficient.",
+  icons: {
+    icon: "/assets/icon.png",
+  },
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <DocSidebar />
-      <main className="w-full">
-        <SidebarTrigger className="sticky top-16" />
-        <DocsLayout>{children}</DocsLayout>
-      </main>
-    </SidebarProvider>
-  );
+  return <DocsLayout>{children}</DocsLayout>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import { Header } from "../components/common/header";
 import "@prex0/uikit/styles.css";
 import "./global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Prex - Revolutionize DApp Development -",
+  viewport: "width=device-width, initial-scale=1.0",
+  description:
+    "Prex is a platform for building decentralized applications (DApps) on the Ethereum blockchain. It provides a suite of tools and services to streamline the development process and make it more efficient.",
+  icons: {
+    icon: "/assets/icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -9,16 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/assets/icon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Prex - Revolutionize DApp Development -</title>
-        <meta
-          name="description"
-          content="Prex is a platform for building decentralized applications (DApps) on the Ethereum blockchain. It provides a suite of tools and services to streamline the development process and make it more efficient."
-        />
-      </head>
+      <head></head>
       <body className="dark">
         <div id="root">
           <div className="min-h-screen bg-black text-white flex flex-col">
